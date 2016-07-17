5d81ad0dd65ca8ef6d06446826a80caf224f1c95
JP <jp@bile.localdomain>
2016-07-18 06:07:57 +1000

    add ROLM base image

---
+++ vue/footer.php
@@ -0,0 +1,8 @@
+<?php
+// DEBUG
+echo '<div class="debug"><pre><p>DEBUG</p>';
+echo '<p>GET    : ' . var_dump($_GET) . '</p>';
+echo '<p>POST   : ' . var_dump($_POST) . '</p>';
+echo '<p>SESSION: ' . var_dump($_SESSION) . '</p>';
+echo '</pre></div>';
+?>
